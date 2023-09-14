e179330d7fb771f1efad1e8302c8b823b329a0e8
maxim_stoma <maksim.stoma@mail.ru>
2023-09-15 02:14:38 +0300

    edited Main class (repeated seminar 2 work)

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
+    private static Random random = new Random();
+
+    public static Worker generateWorker() {
+        String[] names = new String[] {"Александр", "Дмитрий", "Иван", "Михаил", "Сергей", "Андрей", "Алексей", "Егор", "Максим", "Никита"};
+        String[] surnames = new String[] {"Иванов", "Петров", "Смирнов", "Соколов", "Кузнецов", "Морозов", "Новиков", "Козлов", "Лебедев", "Зайцев"};
+
+        int salaryIndex = random.nextInt(200, 500);
+
+        return new Worker(names[random.nextInt(names.length)], surnames[random.nextInt(surnames.length)], 100 * salaryIndex);
+    }
+
+    public static Worker[] generateWorkers(int count) {
+        Worker[] workers = new Worker[count];
+        for (int i = 0; i < count; i++) {
+            workers[i] = generateWorker();
+        }
+        return workers;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Worker[] workers = generateWorkers(12);
+        for (Worker worker : workers) {
+            System.out.println(worker);
+        }
+
+        Arrays.sort(workers/*, new SalaryComparator()*/);
+        System.out.println("========================================");
+        for (Worker worker : workers) {
+            System.out.println(worker);
+        }
     }
 }
