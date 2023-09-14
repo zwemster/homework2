777ed1a21f9d672fabd5ecdfda66bdb100045dda
maxim_stoma <maksim.stoma@mail.ru>
2023-09-15 02:12:55 +0300

    implemented SalaryComparator class

---
+++ SalaryComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class SalaryComparator implements Comparator<Employee> {
+    @Override
+    public int compare(Employee o1, Employee o2) {
+
+        return Double.compare(o1.salaryCalculate(), o2.salaryCalculate());
+    }
+}
