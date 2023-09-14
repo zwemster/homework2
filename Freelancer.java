93c9424766df849aa6264b9d0bcd91f66a8c849d
maxim_stoma <maksim.stoma@mail.ru>
2023-09-15 02:11:13 +0300

    implemented Freelancer class

---
+++ Freelancer.java
@@ -0,0 +1,21 @@
+public class Freelancer extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public Freelancer(String name, String surName, double salary, int hoursWorked, double hourlyRate) {
+        super(name, surName, salary);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double salaryCalculate() {
+        return hoursWorked * hourlyRate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Фрилансер: %s %s. Среднемесячная заработная плата (почасовая): %.2f",
+                surName, name, salaryCalculate());
+    }
+}
